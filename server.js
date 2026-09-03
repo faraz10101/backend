94123f91fa16295b598bb8d0f27c47d9602954f0
Faraz_Ahmed <faraz454092@gmail.com>
2026-09-03 07:09:56 +0500

    setup things

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const connectDB = require('./src/db/db');
 
 connectDB();
 
-app.listen(5000, () => {
-    console.log('Server is running on port 5000');
-})
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
